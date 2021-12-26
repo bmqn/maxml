f1c6a4db8e2ee62847f49ffc70aae6bc3a32570a
Max <18558188+maxbryarsmansell@users.noreply.github.com>
2021-12-26 14:34:49 +0000

    Add helper functions to get references to input and output data + delta

--- include/mocr/Sequential.h
+++ include/mocr/Sequential.h
@@ -22,6 +22,14 @@ namespace mocr
 		MSE
 	};
 
+	class SequentialDesc
+	{
+	private:
+		unsigned int m_InputChannels;
+		unsigned int m_InputRows;
+		unsigned int m_InputCols;
+	};
+
 	class Sequential
 	{
 	public:
@@ -47,11 +55,22 @@ namespace mocr
 		double feedBackward(const DTensor& expected);
 
 	private:
+		const DTensor& dataInputAt(unsigned int index) const;
+		const DTensor& dataOutputAt(unsigned int index) const;
+		const DTensor& deltaInputAt(unsigned int index) const;
+		const DTensor& deltaOutputAt(unsigned int index) const;
+
+		DTensor& dataInputAt(unsigned int index);
+		DTensor& dataOutputAt(unsigned int index);
+		DTensor& deltaInputAt(unsigned int index);
+		DTensor& deltaOutputAt(unsigned int index);
+
+	private:
 		unsigned int m_InChannels, m_InRows, m_InCols;
 
 		// Each layer has a pair of tensors for input and output, respectively
 		std::vector<std::pair<std::shared_ptr<DTensor>, std::shared_ptr<DTensor>>> m_Data;
-		std::vector<std::pair<std::shared_ptr<DTensor>, std::shared_ptr<DTensor>>> m_Deltas;
+		std::vector<std::pair<std::shared_ptr<DTensor>, std::shared_ptr<DTensor>>> m_Delta;
 		std::vector< std::shared_ptr<Layer>> m_Layers;
 
 		LossFunc m_ObjectiveFunc;
--- src/Sequential.cpp
+++ src/Sequential.cpp
@@ -21,61 +21,12 @@ namespace mocr
     {
     }
 
-    const DTensor& Sequential::feedForward(const DTensor &input)
-    {
-        for (auto it = m_Layers.cbegin(); it != m_Layers.cend(); ++it)
-        {
-            auto currentLayer = *it;
-
-            if (it == m_Layers.cbegin())
-            {
-                // TODO: The input for the first layer is always unused currently.
-                //       It would be best to use some kind of special 'input' layer.
-                currentLayer->forward(input, *m_Data[0].second);
-            }
-            else
-            {
-                unsigned int currIndex = it - m_Layers.cbegin();
-                currentLayer->forward(*m_Data[currIndex].first, *m_Data[currIndex].second);
-            }
-        }
-
-        return *m_Data.back().second;
-    }
-
-    double Sequential::feedBackward(const DTensor &expected)
-    {
-        if (m_ObjectiveFunc == LossFunc::MSE)
-        {
-            // Mean Square Error ...
-
-            DTensor::sub(*m_Data.back().second, expected, *m_Deltas.back().second);
-
-            for (auto it = m_Layers.crbegin(); it != m_Layers.crend() - 1; ++it)
-            {
-                auto currentLayer = *it;
-
-                unsigned int currIndex = m_Layers.crend() - it - 1;
-                currentLayer->backward(*m_Data[currIndex].first, *m_Data[currIndex].second, *m_Deltas[currIndex].first, *m_Deltas[currIndex].second);
-                
-                currentLayer->update(m_LearningRate);
-            }
-
-            auto error = DTensor::sumWith(*m_Deltas.back().second, [](double x) { return x * x; }) * 0.5;
-            return error;
-        }
-        else
-        {
-            return std::numeric_limits<double>::infinity();
-        }
-    }
-
     void Sequential::addFullyConnectedLayer(int connections, ActivationFunc activation)
     {
         // TODO: validate the previous layer before allowing input
 
         // TODO: Introduce some kind of 'input' layer
-        auto inputs  = m_Layers.size() == 0 ? m_InRows : m_Layers.back()->Rows;
+        auto inputs = m_Layers.size() == 0 ? m_InRows : m_Layers.back()->Rows;
         auto outputs = connections;
 
         if (m_Layers.size() == 0)
@@ -84,7 +35,7 @@ namespace mocr
             m_Data.push_back(std::make_pair(nullptr, output));
 
             std::shared_ptr<DTensor> outputDelta = std::make_shared<DTensor>(1, outputs, 1);
-            m_Deltas.push_back(std::make_pair(nullptr , outputDelta));
+            m_Delta.push_back(std::make_pair(nullptr, outputDelta));
         }
         else
         {
@@ -92,9 +43,9 @@ namespace mocr
             std::shared_ptr<DTensor> output = std::make_shared<DTensor>(1, outputs, 1);
             m_Data.push_back(std::make_pair(input, output));
 
-            std::shared_ptr<DTensor> inputDelta = m_Deltas.back().second;
+            std::shared_ptr<DTensor> inputDelta = m_Delta.back().second;
             std::shared_ptr<DTensor> outputDelta = std::make_shared<DTensor>(1, outputs, 1);
-            m_Deltas.push_back(std::make_pair(inputDelta, outputDelta));
+            m_Delta.push_back(std::make_pair(inputDelta, outputDelta));
         }
 
         std::random_device rd;
@@ -113,11 +64,115 @@ namespace mocr
             std::shared_ptr<DTensor> output = std::make_shared<DTensor>(1, outputs, 1);
             m_Data.push_back(std::make_pair(input, output));
 
-            std::shared_ptr<DTensor> inputDelta = m_Deltas.back().second;
+            std::shared_ptr<DTensor> inputDelta = m_Delta.back().second;
             std::shared_ptr<DTensor> outputDelta = std::make_shared<DTensor>(1, outputs, 1);
-            m_Deltas.push_back(std::make_pair(inputDelta, outputDelta));
+            m_Delta.push_back(std::make_pair(inputDelta, outputDelta));
         }
 
         m_Layers.push_back(std::make_shared<ActvLayer>(1, connections, 1, activation));
+    }
+
+    const DTensor& Sequential::feedForward(const DTensor &input)
+    {
+        for (auto it = m_Layers.cbegin(); it != m_Layers.cend(); ++it)
+        {
+            auto currentLayer = *it;
+
+            if (it == m_Layers.cbegin())
+            {
+                // TODO: The input for the first layer is always unused currently.
+                //       It would be best to use some kind of special 'input' layer.
+                currentLayer->forward(input, dataOutputAt(0));
+            }
+            else
+            {
+                unsigned int currIndex = it - m_Layers.cbegin();
+                currentLayer->forward(dataInputAt(currIndex), dataOutputAt(currIndex));
+            }
+        }
+
+        return *m_Data.back().second;
+    }
+
+    double Sequential::feedBackward(const DTensor &expected)
+    {
+        if (m_ObjectiveFunc == LossFunc::MSE)
+        {
+            // Mean Square Error ...
+            DTensor::sub(dataOutputAt(m_Layers.size() - 1), expected, deltaOutputAt(m_Layers.size() - 1));
+
+            for (auto it = m_Layers.crbegin(); it != m_Layers.crend() - 1; ++it)
+            {
+                auto currentLayer = *it;
+
+                unsigned int currIndex = m_Layers.crend() - it - 1;
+                currentLayer->backward(dataInputAt(currIndex), dataOutputAt(currIndex), deltaInputAt(currIndex), deltaOutputAt(currIndex));
+                
+                currentLayer->update(m_LearningRate);
+            }
+
+            auto error = DTensor::sumWith(deltaOutputAt(m_Layers.size() - 1), [](double x) { return x * x; }) * (1.0 / deltaOutputAt(m_Layers.size() - 1).rows());
+            return error;
+        }
+        else
+        {
+            return std::numeric_limits<double>::infinity();
+        }
+    }
+
+    const DTensor& Sequential::dataInputAt(unsigned int index) const
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Data[index].first);
+    }
+
+    const DTensor& Sequential::dataOutputAt(unsigned int index) const
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Data[index].second);
+    }
+
+    const DTensor& Sequential::deltaInputAt(unsigned int index) const
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Delta[index].first);
+    }
+
+    const DTensor& Sequential::deltaOutputAt(unsigned int index) const
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Delta[index].second);
+    }
+
+    DTensor& Sequential::dataInputAt(unsigned int index)
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Data[index].first);
+    }
+
+    DTensor& Sequential::dataOutputAt(unsigned int index)
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Data[index].second);
+    }
+
+    DTensor& Sequential::deltaInputAt(unsigned int index)
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Delta[index].first);
+    }
+
+    DTensor& Sequential::deltaOutputAt(unsigned int index)
+    {
+        assert(index >= 0 && index < m_Layers.size());
+
+        return *(m_Delta[index].second);
     }
 }
